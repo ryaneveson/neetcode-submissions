828f405064fc45ce8e14be47adbd9e716893fafe
ryaneveson <122495897+ryaneveson@users.noreply.github.com>
2026-09-24 10:48:36 -0600

    Add: valid-palindrome-ii - submission-4

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-4.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean validPalindrome(String s) {
+        int l = 0;
+        int r = s.length()-1;
+        while(r>l){
+            if(s.charAt(l) != s.charAt(r)){
+                return isPalindrome(s.substring(0,l)+s.substring(l+1)) || isPalindrome(s.substring(0,r) + s.substring(r+1));
+            }
+            l++;
+            r--;
+        }
+        return true;
+    }
+
+        private boolean isPalindrome(String s){
+            int l = 0;
+            int r = s.length()-1;
+            while(r>l){
+                if(s.charAt(l) != s.charAt(r)){
+                    return false;
+                }
+                l++;
+                r--;
+            }
+            return true;
+        }
+}
